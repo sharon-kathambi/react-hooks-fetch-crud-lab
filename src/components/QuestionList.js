3fba53e99f1d9ffab5a11d781843c536042f2a98
Sharon Mwenda <smkathambi@gmail.com>
2022-07-23 14:33:15 +0300

    cleaning up code

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -3,17 +3,9 @@ import QuestionItem from "./QuestionItem"
 
 function QuestionList({mydata, deleteQuestion, updatedQuestion}) {
 
-  //const [questions, setQuestions] = useState([])
-
   const displayData = mydata.map((val)=>{
     return <QuestionItem key={val.id} question ={val} deleteQuestion={deleteQuestion} updatedQuestion={updatedQuestion}/>
   })
-
-  /*useEffect(() => {
-    fetch("http://localhost:4000/questions")
-    .then(response => response.json())
-    .then(questions => setQuestions(questions))
-  }, [])*/
 
   return (
     <section>
@@ -26,7 +18,3 @@ function QuestionList({mydata, deleteQuestion, updatedQuestion}) {
 }
 
 export default QuestionList;
-
-/*{questions.map((question) => (
-  <QuestionItem key= {question.id} question={question} />
-))}*/
